1b713cc042dab73b5f55dea051fb43661b052f2e
Thamizhselvan21 <thamizhselvan2106@gmail.com>
2025-07-23 11:57:11 +0530

    from selvan

--- placementapp/src/day1/Placement.java
+++ placementapp/src/day1/Placement.java
@@ -3,4 +3,17 @@ package day1;
 public class Placement {
 	private int pid;
 	private String job;
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	
 }
